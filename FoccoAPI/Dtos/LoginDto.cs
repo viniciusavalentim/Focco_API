de1563b30968109a8dbd64666b03db9e4679ceac
vinicius valentim <viniciusvalentim769@gmail.com>
2024-10-02 14:55:41 -0300

    Alterando nomenclatura, adicionando require nos DTOs de criaçào e de login e colocando textos de validações.

--- FoccoAPI/Dtos/LoginDto.cs
+++ FoccoAPI/Dtos/LoginDto.cs
@@ -1,8 +1,13 @@
-﻿namespace FoccoAPI.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FoccoAPI.Dtos
 {
     public class LoginDto
     {
+        [Required(ErrorMessage = "O campo Email é obgratório"), EmailAddress(ErrorMessage = "Email inválido")]
         public string? Email { get; set; }
+
+        [Required(ErrorMessage = "O campo Senha é obgratório")]
         public string? Password { get; set; }
     }
 }
--- FoccoAPI/Dtos/RegisterDto.cs
+++ FoccoAPI/Dtos/RegisterDto.cs
@@ -4,18 +4,17 @@ namespace FoccoAPI.Dtos
 {
     public class RegisterDto
     {
-        [Required(ErrorMessage = "O campo User é obgratorio")]
+        [Required(ErrorMessage = "O campo User é obgratório")]
         public string? User { get; set; }
 
-        [Required(ErrorMessage = "O campo Email é obgratorio"), EmailAddress(ErrorMessage = "Inválid Email")]
+        [Required(ErrorMessage = "O campo Email é obgratório"), EmailAddress(ErrorMessage = "Email inválido, tente novamente")]
         public string? Email { get; set; }
 
-        [Required(ErrorMessage = "O campo Password é obgratorio")]
+        [Required(ErrorMessage = "O campo Senha é obgratório")]
         public string? Password { get; set; }
 
         [Compare("Password", ErrorMessage = "Senhas nao coincidem")]
         public string? ConfirmPassword { get; set; }
-
 
     }
 }
